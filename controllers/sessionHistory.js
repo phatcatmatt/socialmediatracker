c99a1f76392638a29bfdd355b1abc87fac4c31b5
Matthew Nicholas <phatcatmatt@gmail.com>
2016-05-23 00:17:29 -0600

    ref logs in front end but still doesn't save to db

--- controllers/sessionHistory.js
+++ controllers/sessionHistory.js
@@ -3,18 +3,23 @@ var sessions = require('../models/sessions')
 module.exports = {
 
   updateOrAddHistory: function(req, res, next) {
-    console.log(req.body);
     sessions.findByIdAndUpdate(
     req.sessionID,
-    {$push:{'history': req.body._id}},
+    {$addToSet:{'history': req.body._id}}, {'new': true, upsert: true},
      function(err, s) {
       if (err){
         console.log(err);
         res.status(500).send(err)
       } else {
-        console.log('we doin this');
-        console.log(s.history);
-        res.send(200);
+        // console.log('history ', s);
+        s.save(function(err2, s2) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log('suksks ', s2);
+            res.send(s2)
+          }
+        })
         // if (s.searchHistory) {
         //   s.searchHistory.push()
         // }
