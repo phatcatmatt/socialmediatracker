771ab19582910615e1320a2f6665be3f75342180
Matthew Nicholas <phatcatmatt@gmail.com>
2016-07-11 11:30:27 -0600

    updated db connection

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json({limit: '50mb'}));
 
 
 mongoose.set('debug', true)
-mongoose.connect('mongodb://localhost:27017/trackFollowers', function(err) {
+mongoose.connect('mongodb://127.0.0.1:27017/trackFollowers', function(err) {
     if (err) {
         console.log(err)
     }
